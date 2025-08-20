02ed38aadb81e18044db701ec49df872b12f52f1
Vinícius Fritzen Machado <fritzenvinicius@gmail.com>
2025-08-19 21:20:15 -0300

    Add files via upload

--- Aula02/src/index.js
+++ Aula02/src/index.js
@@ -3,55 +3,96 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
 
 const products = [
     {
         id: 1,
-        name: 'Motherboard',
-        price: 250
+        name: 'B660M',
+        brand: "ASUS",
+        price: 250,
+        category: "Motherboard"
     },
     {
         id: 2,
-        name: 'CPU',
-        price: 300
+        name: 'i5',
+        brand: "INTEL",
+        price: 300,
+        category: "CPU"
     },
     {
         id: 3,
-        name: 'RAM',
-        price: 150
+        name: '16GB DDR4',
+        brand: 'CORSAIR',
+        price: 150,
+        category: 'Memory'
     },
     {
         id: 4,
         name: 'GPU 1',
-        price: 400
+        brand: 'AMD',
+        price: 400,
+        category: 'GPU',
     },
     {
         id: 5,
         name: 'GPU 2',
-        price: 450
+        brand: 'GFORCE',
+        price: 450,
+        category: 'GPU',
     }
 ];
 
 /*
 PAGINAS ESTATICAS
 */
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+    // res.send('Hello World!');
+    // res.render('');
+    // res.send(arquivo)
+// });
 
 /*
 TEMPLATES
 */
+
+app.get('/products', (req, res) => {
+    // res.json(products);
+    res.render('products-list', { lista: products });
+});
+
+// req, res
+// request, response    sao palavras normalmente utilizadas, mas podemos escolher
+app.get('/products/:id', (req, res) => {
+    // :id => parametro da rota
+    // ao acessar o produto 1, quero ver os detalhes do produto 1
+
+    // const id = req.params.id;
+    const { id } = req.params;
+
+                            // filter é um método para filtrar arrays em JS
+    const produtoComId = products.filter(p => p.id == id)[0];
+
+    res.render('products-detail', { product: produtoComId});
+
+});
+
+// posso ter quantos parametros eu quiser
+// noticia/:regiao/:data/:titulo
 
 /*
 Rotas com Parâmetros e Query Params
 
 Crie um endpoint GET /produtos/:id que retorna detalhes de um produto.
 
-Crie um endpoint GET /busca que recebe um query param nome e retorna apenas os produtos que contêm esse nome.
+Crie um endpoint GET /busca que recebe um query param nome e retorna apenas os produtos que contêm esse nome ou essa categoria.
 
 Exemplo:
-GET /busca?nome=camisa
+GET /busca?nome=gpu => retorna a lista com 2 produtos
 */
 
 
---
+++ Aula02/src/teste.js
@@ -0,0 +1,12 @@
+let a = [1, 3, 4]
+console.log({ a })
+
+
+a.forEach(elem => {
+    
+})
+
+
+a.forEach(function(elem) {
+    
+})
